5676618c9998458d4cf1629e37907b971b2fd89e
Nick Wallin <nick.wallin@sebrands.com>
2016-03-03 19:20:54 -0600

    initial xcodeproj commit

---
+++ Chromatology/Chromatology/Chromatology.h
@@ -0,0 +1,19 @@
+//
+//  Chromatology.h
+//  Chromatology
+//
+//  Created by Nick Wallin on 3/3/16.
+//  Copyright © 2016 nicnacklabratories. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Chromatology.
+FOUNDATION_EXPORT double ChromatologyVersionNumber;
+
+//! Project version string for Chromatology.
+FOUNDATION_EXPORT const unsigned char ChromatologyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Chromatology/PublicHeader.h>
+
+
